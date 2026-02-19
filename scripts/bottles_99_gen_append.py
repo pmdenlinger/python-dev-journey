01e287170eeab152dc9dd4d80f96d042223ef152
Paul Denlinger <pmdenlinger@icloud.com>
2026-02-19 14:50:47 -0800

    Create scripts/bottles_99_gen_append.py, add code and comment

---
+++ scripts/bottles_99_gen_append.py
@@ -0,0 +1,25 @@
+# Define the bottles_song function
+# with the start argument defaulting to 99
+def bottles_song(start=99):
+    # Set the initial number of bottles to the start argument
+    bottles = start
+    # Loop through until bottles are gone
+    while bottles > 0:
+        # Display the song
+        this_verse = []
+        this_verse.append(str(bottles) + " bottles of beer on the wall.\n ")
+        this_verse.append(str(bottles) + " bottles of beer.\n ")
+        this_verse.append("Take one down, pass it around.\n ")
+        # Subtract a bottle
+        bottles -= 1
+        this_verse.append(str(bottles) + " bottles of beer on the wall. ") 
+        # Yield to the calling function
+        yield "".join(this_verse)
+    # Pick back up here when we return
+    return True
+
+# Loop through the generator
+for v in bottles_song():
+    print(v)
+
+# this_verse is initialized as an empty array, and each loop creates a string which is appended to the array. Finally, it is printed. More # # efficient in use of system resources than other versions.   
