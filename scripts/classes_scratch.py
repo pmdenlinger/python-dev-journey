c3d9862e2e2758d39d1a7d9c0f956055eab84bcf
Paul Denlinger <pmdenlinger@icloud.com>
2026-02-20 14:29:12 -0800

    Create new class with explicit __init__ call for name, then add name Robert to new name instance

--- scripts/classes_scratch.py
+++ scripts/classes_scratch.py
@@ -41,15 +41,29 @@
 # print(World.greeting)    
 
 # This variable exists in the main scope
+# name = "Sarah"
+
+# Define a new class with a class variable called name
+# class Customer:
+#     name = "Robert"
+
+# Create a new customer so that __init__ is called
+# customer = Customer()
+
+# Display the nmae in the main scope
+# print(customer.name)
+
+# This variable exists in the main scope
 name = "Sarah"
 
 # Define a new class with a class variable called name
 class Customer:
-    name = "Robert"
+    def __init__(self, name):
+        self.name = name
 
 # Create a new customer so that __init__ is called
-customer = Customer()
+customer = Customer("Robert")
 
-# Display the nmae in the main scope
+# Display the name in the main scope
 print(customer.name)
 
