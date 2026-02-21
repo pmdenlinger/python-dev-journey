440ecd297073c6639ebaaf8d3ea22e436ce8e949
Paul Denlinger <pmdenlinger@icloud.com>
2026-02-21 11:48:52 -0800

    Copilot does final cleaing up on code

--- scripts/06_coffee.py
+++ scripts/06_coffee.py
@@ -13,9 +13,15 @@ def prompt(display="Please input a string", require=True):
     if require:
         s = False
         while not s:
-            s = input(display + " ")
+            try:
+                s = input(display + " ")
+            except EOFError:
+                return ""
     else:
-        s = input(display + " ")
+        try:
+            s = input(display + " ")
+        except EOFError:
+            return ""
     return s
 
 def convert_to_float(s):
@@ -29,9 +35,9 @@ def convert_to_float(s):
 def x_of_y(x, y):
     num_list = []
     # Return a list of x numbers of y
-    for i in range(x):
+    for _ in range(x):
         num_list.append(y)
-        return num_list
+    return num_list
     
 class CoffeeShopSimulator:
 
@@ -66,111 +72,151 @@ class CoffeeShopSimulator:
         # The main game loop
         running = True
         while running:
-        # Display the day and add a "fancy" text effect
+            # Display the day and add a "fancy" text effect
             self.day_header()
-        
 
-        # Get the weather
-        temperature = self.weather
+            if self.coffee_inventory <= 0:
+                print("You're out of coffee. Please restock to keep playing.")
+            # Get the weather
+            temperature = self.weather
 
-        # Display the cash and weather
-        self.daily_stats(temperature)
+            # Display the cash and weather
+            self.daily_stats(temperature)
 
-        # Get price of a cup of coffee
-        cup_price = float(
-            prompt("What do you want to charge per cup of coffee?"))
-        
-        # Get advertising spend
-        print("You can buy advertising to help promote sales.")
-        advertising = prompt("How much do you want to spend on advertising (0 for none)?",  False)
+            # Get price of a cup of coffee
+            cup_price_input = prompt("What do you want to charge per cup of coffee?")
+            if cup_price_input == "":
+                print("\nNo input received. Exiting game.")
+                break
+            cup_price = convert_to_float(cup_price_input)
 
-        # Convert advertising into a float
-        advertising = convert_to_float(advertising)
+            # Get advertising spend
+            print("You can buy advertising to help promote sales.")
+            advertising_input = prompt("How much do you want to spend on advertising (0 for none)?", False)
+            if advertising_input == "":
+                print("\nNo input received. Exiting game.")
+                break
 
-        # Deduct advertising from cash on hand
-        self.cash -= advertising
+            # Convert advertising into a float
+            advertising = convert_to_float(advertising_input)
 
-        # Simulate today's sales
-        cups_sold = self.simulate(temperature, advertising, cup_price)
-        gross_profit = cups_sold * cup_price
+            # Deduct advertising from cash on hand
+            self.cash -= advertising
 
-        # Display the results
-        print("You sold " + str(cups_sold) + " cups of coffee today.")
-        print("You made $" + str(gross_profit) + ".")
+            # Simulate today's sales
+            cups_sold = self.simulate(temperature, advertising, cup_price)
+            gross_profit = cups_sold * cup_price
 
-        # Add the profit to our coffers
-        self.cash += gross_profit
+            # Display the results
+            print("You sold " + str(cups_sold) + " cups of coffee today.")
+            print("You made $" + str(gross_profit) + ".")
 
-        # Subtract inventory
-        self.coffee_inventory -= cups_sold
+            # Add the profit to our coffers
+            self.cash += gross_profit
 
-        # Before we loop around, add a day
-        day += 1  
+            # Subtract inventory
+            self.coffee_inventory -= cups_sold
 
-def simulate(self, temperature, advertising, cup_price):
-    # Find out how many cups were sold
-    cups_sold = self.daily_sales(temperature, advertising)
+            # Before we loop around, add a day
+            self.increment_day()
 
-    # Save the sales data for today
-    self.sales.append({
-        "day": self.day,
-        "coffee_inv": self.coffee_inventory,
-        "advertising": advertising,
-        "temp": temperature,
-        "cup_price": cup_price,
-        "cups_sold": cups_sold
-    })
+            # Ask if the player wants to continue, restock, or quit
+            print("Options: p = play again, r = restock, q = quit")
+            next_action = prompt("Play again, restock, or quit? (p/r/q)", False)
+            if next_action == "":
+                print("\nNo input received. Exiting game.")
+                break
 
-    # We technically don't need this, but why make the next step
-    # read from the sales list when we have the data right here
-    return cups_sold
+            next_action = next_action.strip().lower()
 
-# Faux temp distributions. We'll do this better
-# later with a bell curve, but for now a quick hack
+            if next_action == "q":
+                running = False
+            elif next_action == "r":
+                restock_input = prompt("How many cups would you like to restock?", False)
+                if restock_input == "":
+                    print("\nNo input received. Exiting game.")
+                    break
 
-def make_temp_distribution(self):
-    # This is not a good bell curve, but it will do for now
-    # until we get to more advanced mathematics
-    temps = []
+                restock_amount = int(convert_to_float(restock_input))
+                if restock_amount > 0:
+                    self.coffee_inventory += restock_amount
+                    print("Inventory restocked by " + str(restock_amount) + " cups.")
+                else:
+                    print("Restock amount must be greater than 0. Exiting game.")
+                    running = False
+            elif next_action == "p":
+                if self.coffee_inventory <= 0:
+                    print("You're out of coffee. Please restock next time. Game over!")
+                    running = False
+            else:
+                print("Unrecognized option. Exiting game.")
+                running = False
 
-    # First, find the average between TEMP_MIN and TEMP_MAX
-    avg = (self.TEMP_MIN + self.TEMP_MAX) / 2
-    # Find the distance between TEMP_MAX and the average
-    max_dist_from_avg = self.TEMP_MAX - avg
+    def simulate(self, temperature, advertising, cup_price):
+        # Find out how many cups were sold
+        cups_sold = self.daily_sales(temperature, advertising)
+        cups_sold = max(0, min(cups_sold, self.coffee_inventory))
 
-    # Loop through all possible temperatures
-    for i in range(self.TEMP_MIN, self.TEMP_MAX):
-        # How far away is the temperature from average?
-        # abs() gives us the absolute value
-        dist_from_avg = abs(avg - i)
-        # How far away is the dist_from_avg from the maximum?
-        # This will be lower for temps at the extremes
-        dist_from_max_dist == max_dist_from_avg - dist_from_avg
-        # If the value is zero, make it one
-        if dist_from_max_dist == 0:
-            dist_from_max_dist = 1
-        # Append the output of x_of_y to temps
-        for t in x_of_y(int(dist_from_max_dist), i):
-            temps.append(t)
-    return temps
-def increment_day(self):
-    self.day += 1
+        # Save the sales data for today
+        self.sales.append({
+            "day": self.day,
+            "coffee_inv": self.coffee_inventory,
+            "advertising": advertising,
+            "temp": temperature,
+            "cup_price": cup_price,
+            "cups_sold": cups_sold
+        })
 
-def daily_stats(self, temperature):
-    print("You have $" + str(self.cash) + " cash on hand and the temperature is " + str(temperature) + ".")
-    print("You have enough coffee on hand to make " + str(self.coffee_inventory) + " cups.\n") 
+        # We technically don't need this, but why make the next step
+        # read from the sales list when we have the data right here
+        return cups_sold
 
-def day_header(self):
-    print("\n-----| Day " + str(self.day) + " @ " + self.shop_name + " |-----")
+    # Faux temp distributions. We'll do this better
+    # later with a bell curve, but for now a quick hack
+    def make_temp_distribution(self):
+        # This is not a good bell curve, but it will do for now
+        # until we get to more advanced mathematics
+        temps = []
 
-def daily_sales(self, temperature, advertising):
-    return int((self.TEMP_MAX - temperature) * (advertising * 0.5))
+        # First, find the average between TEMP_MIN and TEMP_MAX
+        avg = (self.TEMP_MIN + self.TEMP_MAX) / 2
+        # Find the distance between TEMP_MAX and the average
+        max_dist_from_avg = self.TEMP_MAX - avg
 
-@property
-def weather(self):
-    # Generate a random temperature between 20 and 90
-    # We'll consider seasons later on, but this is good enough for now
-    return random.choice(self.temps)
+        # Loop through all possible temperatures
+        for i in range(self.TEMP_MIN, self.TEMP_MAX + 1):
+            # How far away is the temperature from average?
+            # abs() gives us the absolute value
+            dist_from_avg = abs(avg - i)
+            # How far away is the dist_from_avg from the maximum?
+            # This will be lower for temps at the extremes
+            dist_from_max_dist = max_dist_from_avg - dist_from_avg
+            # If the value is zero, make it one
+            if dist_from_max_dist == 0:
+                dist_from_max_dist = 1
+            # Append the output of x_of_y to temps
+            for t in x_of_y(int(dist_from_max_dist), i):
+                temps.append(t)
+        return temps
+
+    def increment_day(self):
+        self.day += 1
+
+    def daily_stats(self, temperature):
+        print("You have $" + str(self.cash) + " cash on hand and the temperature is " + str(temperature) + ".")
+        print("You have enough coffee on hand to make " + str(self.coffee_inventory) + " cups.\n")
+
+    def day_header(self):
+        print("\n-----| Day " + str(self.day) + " @ " + self.shop_name + " |-----")
+
+    def daily_sales(self, temperature, advertising):
+        return int((self.TEMP_MAX - temperature) * (advertising * 0.5))
+
+    @property
+    def weather(self):
+        # Generate a random temperature between 20 and 90
+        # We'll consider seasons later on, but this is good enough for now
+        return random.choice(self.temps)
 
 # Print welcome message
 welcome()
