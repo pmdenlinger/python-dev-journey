1f1edf5da9e07838500418270541149e3a50c6ca
Paul Denlinger <pmdenlinger@icloud.com>
2026-02-21 10:21:13 -0800

    Finish code for CoffeeShopSimulator including class and properties

--- scripts/06_coffee.py
+++ scripts/06_coffee.py
@@ -67,8 +67,8 @@ class CoffeeShopSimulator:
         running = True
         while running:
         # Display the day and add a "fancy" text effect
-        self.day_header()
-        # print("\n-----| Day " + str(day) + " @ " + shop_name + " |-----")
+            self.day_header()
+        
 
         # Get the weather
         temperature = self.weather
@@ -107,34 +107,83 @@ class CoffeeShopSimulator:
         # Before we loop around, add a day
         day += 1  
 
+def simulate(self, temperature, advertising, cup_price):
+    # Find out how many cups were sold
+    cups_sold = self.daily_sales(temperature, advertising)
 
-# Current day number 
-day = 1
+    # Save the sales data for today
+    self.sales.append({
+        "day": self.day,
+        "coffee_inv": self.coffee_inventory,
+        "advertising": advertising,
+        "temp": temperature,
+        "cup_price": cup_price,
+        "cups_sold": cups_sold
+    })
 
-# Starting cash on hand
-cash = 100.00
+    # We technically don't need this, but why make the next step
+    # read from the sales list when we have the data right here
+    return cups_sold
 
-# Coffee on hand (cups)
-coffee = 100
+# Faux temp distributions. We'll do this better
+# later with a bell curve, but for now a quick hack
 
+def make_temp_distribution(self):
+    # This is not a good bell curve, but it will do for now
+    # until we get to more advanced mathematics
+    temps = []
 
+    # First, find the average between TEMP_MIN and TEMP_MAX
+    avg = (self.TEMP_MIN + self.TEMP_MAX) / 2
+    # Find the distance between TEMP_MAX and the average
+    max_dist_from_avg = self.TEMP_MAX - avg
 
+    # Loop through all possible temperatures
+    for i in range(self.TEMP_MIN, self.TEMP_MAX):
+        # How far away is the temperature from average?
+        # abs() gives us the absolute value
+        dist_from_avg = abs(avg - i)
+        # How far away is the dist_from_avg from the maximum?
+        # This will be lower for temps at the extremes
+        dist_from_max_dist == max_dist_from_avg - dist_from_avg
+        # If the value is zero, make it one
+        if dist_from_max_dist == 0:
+            dist_from_max_dist = 1
+        # Append the output of x_of_y to temps
+        for t in x_of_y(int(dist_from_max_dist), i):
+            temps.append(t)
+    return temps
+def increment_day(self):
+    self.day += 1
 
+def daily_stats(self, temperature):
+    print("You have $" + str(self.cash) + " cash on hand and the temperature is " + str(temperature) + ".")
+    print("You have enough coffee on hand to make " + str(self.coffee_inventory) + " cups.\n") 
 
-def daily_stats(cash_on_hand, weather_temp, coffee_inventory):
-    print("You have $" + str(cash_on_hand) + " cash on hand and the temperature is " + str(weather_temp) + ".")
-    print("You have enough coffee on hand to make " + str(coffee_inventory) + " cups.\n") 
+def day_header(self):
+    print("\n-----| Day " + str(self.day) + " @ " + self.shop_name + " |-----")
 
+def daily_sales(self, temperature, advertising):
+    return int((self.TEMP_MAX - temperature) * (advertising * 0.5))
 
-
-
+@property
+def weather(self):
+    # Generate a random temperature between 20 and 90
+    # We'll consider seasons later on, but this is good enough for now
+    return random.choice(self.temps)
 
 # Print welcome message
 welcome()
 
 # Get name and store name
-name = prompt("What is your name?", True)
-shop_name = prompt("What do you want to name your coffee shop?", True)
+t_name = prompt("What is your name?", True)
+t_shop_name = prompt("What do you want to name your coffee shop?", True)
+
+# Create the game object
+game = CoffeeShopSimulator(t_name, t_shop_name)
+
+# Run the game
+game.run()
 
 
 
