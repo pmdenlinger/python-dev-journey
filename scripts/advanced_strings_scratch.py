1f638e8a74f500255f4d102773b7605ba04f6bca
Paul Denlinger <pmdenlinger@icloud.com>
2026-03-02 14:01:58 -0800

    Add upper() and lower() methods

--- scripts/advanced_strings_scratch.py
+++ scripts/advanced_strings_scratch.py
@@ -1,13 +1,22 @@
 # Create a string
-a = "Hello, World!"
+# a = "Hello, World!"
 
 # Search for "World" in the string
-if a.find("World") != -1:
+# if a.find("World") != -1:
 
     # Replace "World" with "Reader"
-    b = a.replace("World", "Reader")
+    # b = a.replace("World", "Reader")
 
-    # Display the results
-    print(a)
-    print("... was replaced with ...")
-    print(b)
+    # # Display the results
+    # print(a)
+    # print("... was replaced with ...")
+    # print(b)
+
+# Create our string
+title = "Python QuickStart Guide"
+
+# Display it all uppercase
+print(title.upper())
+
+# Display it all lowercase
+print(title.lower())
