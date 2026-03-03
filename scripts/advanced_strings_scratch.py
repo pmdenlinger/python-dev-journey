f1e8785c2bbb51a4515b2021eb567c8606047fe0
Paul Denlinger <pmdenlinger@icloud.com>
2026-03-03 10:12:02 -0800

    Write scripts to check alpha, nummeric, alphanumeric values

--- scripts/advanced_strings_scratch.py
+++ scripts/advanced_strings_scratch.py
@@ -55,10 +55,38 @@
 # print(glossary_list)
 
 # Our glossary terms
-glossary = ['delimiter', 'module', 'package', 'class', 'object']
+# glossary = ['delimiter', 'module', 'package', 'class', 'object']
 
 # The new joined string
-glossary_string = ", ".join(glossary)
+# glossary_string = ", ".join(glossary)
 
 # Display it 
-print(glossary_string)
+# print(glossary_string)
+
+# Ask user for a value
+value = input("Please enter a value: ")
+
+# Check if every character is a number
+# "3102" - True
+# "4111123412341234" - True
+# "04/22/2022" - False
+# "1600 Pennsylvania Avenue" - False
+if value.isnumeric():
+    print("It's a number.")
+
+# Check if every character is a letter
+# Spaces, punctuation, and numbers don't count
+# "Yes" - True
+# "Yes " - False
+# "Yes 3" - False
+# "Yes!" - False
+if value.isalpha():
+    print("It is filled with alphabet characters only")
+
+# Check if the string is alphanumeric (i.e., letters and numbers)
+# "1600 Pennsylvania Avenue" - False
+# "Washington, D.C." - False
+# "Washington DC" - False
+# "Washington" - True
+if value.isalnum():
+    print("It's alphanumeric.")
