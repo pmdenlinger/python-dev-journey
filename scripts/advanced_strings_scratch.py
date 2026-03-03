2d431b123db3cc098b98a1a903ed6ab4abaeb5e4
Paul Denlinger <pmdenlinger@icloud.com>
2026-03-03 10:16:30 -0800

    Import re engine and check for presence of hello in phrase

--- scripts/advanced_strings_scratch.py
+++ scripts/advanced_strings_scratch.py
@@ -64,15 +64,15 @@
 # print(glossary_string)
 
 # Ask user for a value
-value = input("Please enter a value: ")
+# value = input("Please enter a value: ")
 
 # Check if every character is a number
 # "3102" - True
 # "4111123412341234" - True
 # "04/22/2022" - False
 # "1600 Pennsylvania Avenue" - False
-if value.isnumeric():
-    print("It's a number.")
+# if value.isnumeric():
+#     print("It's a number.")
 
 # Check if every character is a letter
 # Spaces, punctuation, and numbers don't count
@@ -80,13 +80,24 @@ if value.isnumeric():
 # "Yes " - False
 # "Yes 3" - False
 # "Yes!" - False
-if value.isalpha():
-    print("It is filled with alphabet characters only")
+# if value.isalpha():
+#     print("It is filled with alphabet characters only")
 
 # Check if the string is alphanumeric (i.e., letters and numbers)
 # "1600 Pennsylvania Avenue" - False
 # "Washington, D.C." - False
 # "Washington DC" - False
 # "Washington" - True
-if value.isalnum():
-    print("It's alphanumeric.")
+# 
+
+# Import the regular expression engine
+import re
+
+# Define our content
+text = "Hello, World!"
+
+# Is "Hello" in our string?
+if re.search("hello", text, re.IGNORECASE):
+    print("hello is in the string")
+else:
+    print("hello isn't in the string.")
