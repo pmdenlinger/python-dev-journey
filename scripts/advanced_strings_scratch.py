cab24c6996e84a7ff94bdd98d94889d816ededd1
Paul Denlinger <pmdenlinger@icloud.com>
2026-03-03 09:54:44 -0800

    Apply join() to make string again

--- scripts/advanced_strings_scratch.py
+++ scripts/advanced_strings_scratch.py
@@ -53,3 +53,12 @@
 
 # Display it
 # print(glossary_list)
+
+# Our glossary terms
+glossary = ['delimiter', 'module', 'package', 'class', 'object']
+
+# The new joined string
+glossary_string = ", ".join(glossary)
+
+# Display it 
+print(glossary_string)
