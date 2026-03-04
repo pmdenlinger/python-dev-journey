b422f4806c40cd28ed235cd420ecf15160e2be94
Paul Denlinger <pmdenlinger@icloud.com>
2026-03-04 13:09:45 -0800

    Use re module to find text in string

--- scripts/advanced_strings_scratch.py
+++ scripts/advanced_strings_scratch.py
@@ -113,3 +113,15 @@
 
 # Print the match
 # print(match.group(0))
+
+# Import the regular expression engine
+import re 
+
+# Define our content
+text = "Hello, World!"
+
+# Is "Hello" in our string?
+if re.search("Hello", text):
+    print("Hello is in the string.")
+else:
+    print("Hello isn't in the string.")
