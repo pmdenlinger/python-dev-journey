b374224535a141488b5716dfbe7c7a0cb880a19c
Paul Denlinger <pmdenlinger@icloud.com>
2026-03-05 14:16:28 -0800

    Use re.sub to substitute spaces with + character

--- scripts/advanced_strings_scratch.py
+++ scripts/advanced_strings_scratch.py
@@ -217,20 +217,29 @@
 #     print("re.search says it has an e in it")
 
 # Import the regular expression engine
+# import re
+
+# Our string
+# test = "The quick brown fox is fast!"
+
+# Split by spaces using the \s metacharacter 
+# Since we want to account for multiple spaces, we add +
+# space_split = re.split("\s+", test)
+# print(space_split)
+
+# Split by word using the non-word metacharacter
+# Since we want to account for multiple
+# non-word characters, we add +
+# word_split = re.split("\W+", test)
+# print(word_split)
+
+# Import the regular expression engine
 import re
 
 # Our string
 test = "The quick brown fox is fast!"
 
-# Split by spaces using the \s metacharacter 
-# Since we want to account for multiple spaces, we add +
-space_split = re.split("\s+", test)
-print(space_split)
-
-# Split by word using the non-word metacharacter
-# Since we want to account for multiple
-# non-word characters, we add +
-word_split = re.split("\W+", test)
-print(word_split)
-    
+# Substitute spaces for +
+plus_test = re.sub("\s+", "+", test)
+print(plus_test)    
 
